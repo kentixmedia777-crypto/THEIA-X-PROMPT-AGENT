b385e14848224b7a848ad19d0c540cafb4ec0619
kentixmedia777-crypto <kentixmedia777@gmail.com>
2026-04-20 19:45:58 +0800

    Update main.py

--- main.py
+++ main.py
@@ -403,7 +403,7 @@ if password_input == ACCESS_PASSWORD:
         model_choice = st.selectbox(
             "Select Generation Engine",
             [
-                "Google: Nano Banana (Gemini 3 Flash)",
+                "Google: Nano Banana 2 (Latest)",
                 "OpenAI GPT-Image 1.5 (Standard)", 
                 "Black Forest Labs: Flux.1 (Highly Photorealistic)",
                 "Stability AI: SDXL (Alternative Style)"
